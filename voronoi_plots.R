fead07ec656f572f9c59048292beda97449bf0a8
Greta Timaite <gretatimaite@gmail.com>
2022-07-22 09:13:16 +0100

    voronoi tiles as mulitple sf objects

--- voronoi_plots.R
+++ voronoi_plots.R
@@ -89,124 +89,34 @@ tiles_pol = function(deldir_tiles = list()){
   return(list_pol)
 }
 
-del44 = deldir_tiles[[44]] |> tiles_pol()
-deldir_tiles[[44]] |> plot()
-del44[[2]] |> plot(add = T)
-
-for (i in 1:length(deldir_tiles)){
-  list[[i]] = deldir_tiles[[100]] |> tiles_as_pol()
-}
-
-
-# use function to return a list
-# list = list()
-# for (i in 1:length(deldir_tiles)){
-#   j = 1 
-#   if(j <  length(deldir_tiles[[i]]) + 1){
-#     list[[i]] = deldir_tiles[[i]] |> tiles_as_pol()
-#   }
-#   else{
-#     break
-#   }
-# }
-
+# apply function to each frame to polygonise its tiles
 list = list()
 for (i in 1:length(deldir_tiles)){
   if(is.null(deldir_tiles[[i]]) == FALSE){
-  list[[i]] = deldir_tiles[[i]] |> tiles_pol()
+    list[[i]] = deldir_tiles[[i]] |> tiles_pol()
   }
 }
 
+# plot test
 gcs_env_p() 
 deldir_tiles[[130]] |> plot(add = T)
 list[[130]][[1]] |> plot(add = T, col = "red")
-length(list[[1000]])
+
+## now let's join list elements (lists in a frame) into one 
+deldir_pol = list()
+for (i in 1:length(list)){
+  if (is.null(list[[i]]) == FALSE){
+    for (k in 1:length(list[[i]])){
+      deldir_pol[[i]] = do.call(c, list[[i]])
+    }
+  }
+  }
 
 
-# ====== 
-## failure
-
-tiles_as_pol = function(deldir_tiles = list()){
-  tile_coord = data.frame("x" = deldir_tiles$x,
-                          "y" = deldir_tiles$y)
-  
-  tile_sf = tile_coord |> 
-    sf::st_as_sf(coords = c("x", "y"))
-  
-  tile_comb = sf::st_combine(tile_sf)
-  
-  tile_pol = sf::st_cast(tile_comb, 
-                         "POLYGON")
-}
-
-deldir_tiles[[700]] |> tiles_as_pol()
-
-# tile 140 as an sf object 
-tile140 = list("x" = list(deldir_tiles[[140]][[1]][[3]],
-                                 deldir_tiles[[140]][[2]][[3]],
-                                 deldir_tiles[[700]][[3]][[3]]
-                                 ),
-                   "y" = list(deldir_tiles[[140]][[1]][[4]],
-                               deldir_tiles[[140]][[2]][[4]],
-                               deldir_tiles[[700]][[3]][[4]])
-                   )
-tile_list = list()
-for (i in 1:length(tile140[["x"]])){
-  tile_list[[i]] = rbind(tile140[["x"]][[i]],
-                         tile140[["y"]][[i]]) |> 
-    as.data.frame() |> 
-    t()
-  colnames(tile_list[[i]]) = c("x", "y")
-}
-
-tiles_sf = list()
-tiles_comb = list()
-for (i in 1:length(tile_list)){
-  tiles_sf[[i]] = tile_list[[i]] |> 
-    as.data.frame() |> 
-    sf::st_as_sf(coords = c("x", "y"))
-  
-  tiles_comb = lapply(tiles_sf, sf::st_combine) 
-}  
-
-tiles_pol = list()
-for (i in 1:length(tiles_comb)){
-  tiles_pol[[i]] = tiles_comb[[i]] |> sf::st_cast("POLYGON")
-}
-
-tiles_bind = sf::st_union(tiles_pol[[1]], 
-                         tiles_pol[[2]],
-                         tiles_pol[[3]])
-
-tiles_unique = rbind(
-  tile_list[[1]], tile_list[[2]], tile_list[[3]]
-) |> 
-  as.data.frame() |> 
-  dplyr::distinct(.keep_all = T)
-
-tiles_unique_sf = tiles_unique |> 
-  sf::st_as_sf(coords = c("x", "y"))
-tiles_unique_sf |> plot(add = T, col = "red")
-
-tiles_comb1 = sf::st_cast(tiles_bind, "POLYGON")
-tiles_comb[[1]] |> plot()
-
-tiles_pol = tiles_comb[[1]] |>  sf::st_cast("POLYGON")
-tiles_pol |> plot(add = T)
-deldir_tiles[[140]] |> plot()
-tiles_comb[[2]] |>  sf::st_cast("POLYGON") |> plot(add = T, col = "red")
-tiles_comb[[3]] |>  sf::st_cast("POLYGON") |> plot(add = T, col = "blue")
-tiles_bind |> plot(add = T,col = "yellow")
+# plot test
+gcs_env_p() 
+deldir_pol[[1200]] |> plot(add = T)
+deldir_pol[[1200]][1] |> plot(add = T, col = "red")
 
 
-tile140_sf = tile140 |> sf::st_as_sf(coords = c("x", "y"))
 
-
-# tile7 = tile7 |> 
-#   dplyr::mutate(row_id = dplyr::row_number())
-tile7_sf = tile7 |> sf::st_as_sf(coords = c("x", "y"))
-# tile7_point = tile7_sf[1,]
-# tile7_pol = rbind(tile7_sf, tile7_point)
-tile7_pol = sf::st_combine(tile7_sf)
-# tile7_pol = sf::st_cast(tile7_pol, "MULTIPOINT")
-tile7_pol = sf::st_cast(tile7_pol, "POLYGON")
